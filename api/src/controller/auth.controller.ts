8a438187f243237b2c68ba4e8ca290e455c8ac7e
Walber Amorium <walber@refundid.com>
2024-01-21 18:08:23 +1100

    wip

--- api/src/controller/auth.controller.ts
+++ api/src/controller/auth.controller.ts
@@ -32,6 +32,14 @@ export const loginController = async (
   }
 };
 
+const LogoutController = async (req: Request, res: Response) => {
+  try {
+    return res.status(200).json({ message: "success", isAuth: true });
+  } catch (error) {
+    throw new AppError(getErrorMessage(error));
+  }
+}
+
 export const forgotPasswordController = async (req: Request, res: Response) => {
   try {
     const { email } = req.body;
--- client/src/routes/welcome/WelcomePage.tsx
+++ client/src/routes/welcome/WelcomePage.tsx
@@ -1,4 +1,23 @@
+import { useNavigate } from "react-router-dom";
+import { CustomButton } from "../../components/button/Button";
+import Container from "../../components/container/Container";
+import Layout from "../../components/container/Layout";
 
 export const WelcomePage = () => {
-  return <div>Welcome</div>;
+  const navigate = useNavigate();
+  return (
+    <Layout>
+      <Container>
+        <h4 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          Welcome to the app
+        </h4>
+
+        <CustomButton
+          text="LOGOUT"
+          type_="button"
+          onClick={() => navigate("/login")}
+        />
+      </Container>
+    </Layout>
+  );
 };
